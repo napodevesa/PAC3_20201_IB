c21640bb66ca4f8c26ba56df0a856cf48ea90421
ndev <napodevesa@gmail.com>
2020-10-20 18:46:14 +0200

    javadoc2

--- PAC3Ex1/src/edu/uoc/pac3/Address.java
+++ PAC3Ex1/src/edu/uoc/pac3/Address.java
@@ -1,14 +1,33 @@
 package edu.uoc.pac3;
 
+/**
+ * Address Station
+ * 
+ * @author Napoleón Devesa Dalio - ndev
+ * @version 1
+ */
+
 public class Address {
-	
+	/**
+	 * The street name
+	 */
 	public String street = "Sesame Street";
+	/**
+	 * The latitude
+	 */
 
 	public double latitude = 0;
+	/**
+	 * The longitude
+	 */
 	
 	public double longitude = 0;
 
-
+	/** Address constructor
+	 * @param street 	
+	 * @param latitude	
+	 * @param longitude	
+	 */
 	
 	public Address(String street, double latitude, double longitude) throws Exception {
 		
@@ -18,18 +37,39 @@ public class Address {
 			
 	}
 	
+	/**
+	 * Street Getter
+	 * 
+	 * @return street
+	 */
 	public String getStreet() {
 		return street;
 	}
 	
+	/**
+	 * Street Setter
+	 * 
+	 * 
+	 */
+	
 	public void setStreet(String street) {
 		this.street = street;
 	}
-	
+	/**
+	 * Latitude Getter
+	 * 
+	 * @return latitude
+	 */
 
 	public double getLatitude() {
 		return latitude;
 	}
+	
+	/**
+	 * Latitude Setter
+	 * 
+	 * 
+	 */
 
 	public void setLatitude(double latitude) throws Exception {
 		if (latitude < -90 || latitude > 90) {
@@ -38,11 +78,23 @@ public class Address {
 			this.latitude = latitude;
 		}
 	}
+	
+	/**
+	 * Longitude Getter
+	 * 
+	 * @return longitude
+	 */
 
 
 	public double getLongitude() {
 		return longitude;
 	}
+	
+	/**
+	 * Longitude Setter
+	 * 
+	 * 
+	 */
 
 	public void setLongitude(double longitude)  throws Exception {
 		if (longitude < -180 || longitude > 180) {
--- PAC3Ex1/src/edu/uoc/pac3/Station.java
+++ PAC3Ex1/src/edu/uoc/pac3/Station.java
@@ -1,18 +1,56 @@
 package edu.uoc.pac3;
 
+/**
+ * Class Station
+ * 
+ * @author Napoleón Devesa Dalio - ndev
+ * @version 1
+ */
+
+
 public class Station {
-	
+	/**
+	 * The id of the Station
+	 */
 	private int id;
+	/**
+	 * next Id
+	 */
 	private static int nextId = 0;
+	/**
+	 * name of the Station
+	 */
 	private String name = "Default";
+	/**
+	 * Capacity of the Station
+	 */
 	private int capacity = 24;
-	
+	/**
+	 * The address of the Station.
+	 */
 	private Address address;
+	
+	/**
+	 * Constructor without parameters 
+	 * 
+	 * @throws exceptions
+	 */
 	
 	
 	public Station() throws Exception {
 		this("Default", "Sesame Street", 0.0, 0.0, 24);
 	}
+	
+	/**
+	 * Constructor wiht parameters
+	 * 
+	 * @param name      
+	 * @param street    
+	 * @param latitude  
+	 * @param longitude 
+	 * @param capacity  
+	 * @throws Exception 
+	 */
 
 	
 	public Station(String name, String street, double latitude, double longitude, int capacity) throws Exception {
@@ -24,31 +62,62 @@ public class Station {
 		
 	}
 
+	/**
+	 * Id Getter
+	 * 
+	 * @return id
+	 */
 	
 	public int getId() {
 		return id;
 	}
-
+	
+	/**
+	 * Id Setter.
+	 * 
+	 */
 	
 	private void setId() {
 		id = nextId;
 		plusId();
 	}
 
+	/**
+	 * Next Id Getter
+	 * 
+	 * @return nextId
+	 */
 	
 	public static int getNextId() {
 		return nextId;
 	}
+	
+	/**
+	 * next id ++
+	 */
 
 	
 	private void plusId() {
 		nextId++;
 	}
+	
+	/**
+	 * Name Getter.
+	 * 
+	 * @return name of Station
+	 */
 
 	
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Name setter.
+	 * 
+	 * @param name 
+	 * @throws Exception 
+	 */
 
 	
 	public void setName(String name) throws Exception {
@@ -59,11 +128,23 @@ public class Station {
 		}
 	}
 
-	
+
+	/**
+	 * Cap Getter
+	 * 
+	 * @return nextId
+	 */
 	
 	public int getCapacity() {
 		return capacity;
 	}
+	
+
+	/**
+	 * Cap Setter
+	 * 
+	 * 
+	 */
 
 
 	public void setCapacity(int capacity) throws Exception {
@@ -74,11 +155,22 @@ public class Station {
 		}
 	}
 
+	/**
+	 * Address Getter
+	 * 
+	 * @return adress
+	 */
 
 	public Address getAddress() {
 		return this.address;
 	}
-
+	
+	/**
+	 * Address Setter
+	 * @param street	
+	 * @param latitude
+	 * @param longitude	
+	 */
 
 	public void setAddress(String street, double latitude, double longitude) throws Exception {
 		this.address = new Address(street, latitude, longitude);
