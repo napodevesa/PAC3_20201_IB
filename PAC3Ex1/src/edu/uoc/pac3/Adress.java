a369b84202fcf1d6ab1d28627f88d0d5d5495b15
ndev <napodevesa@gmail.com>
2020-10-20 12:38:00 +0200

    pacex1

---
+++ PAC3Ex1/src/edu/uoc/pac3/Adress.java
@@ -0,0 +1,56 @@
+package edu.uoc.pac3;
+
+public class Adress {
+	
+	private String street ;
+
+	private double latitude ;
+	
+	private double longitude ;
+
+
+	
+	public Adress(String street, double latitude, double longitude) throws Exception {
+		
+		setStreet(street);
+		setLatitude(latitude);
+		setLongitude(longitude);
+			
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) throws Exception {
+		if (latitude < -90 || latitude > 90) {
+			throw new Exception("[ERROR] Address latitude must be in range [-90,+90]");
+		} else {
+			this.latitude = latitude;
+		}
+	}
+
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude)  throws Exception {
+		if (longitude < -180 || longitude > 180) {
+			throw new Exception("[ERROR] Address longitude must be in range [-180,+180]");
+		} else {
+			this.longitude = longitude;
+		}
+
+	}
+
+}
---
+++ PAC3Ex1/src/edu/uoc/pac3/Station.java
@@ -0,0 +1,89 @@
+package edu.uoc.pac3;
+
+public class Station {
+	
+	private int id;
+	
+	private static int nextId = 0;
+	
+	private String name = "Default";
+	
+	private int capacity = 24;
+
+	
+	
+	public Station() {
+		setId();
+	}
+
+	
+	public Station(String name, String street, double latitude, double longitude, int capacity) throws Exception {
+
+		setName(name);
+		setCapacity(capacity);
+		setId();
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	
+	private void setId() {
+		id = nextId;
+		incNextId();
+	}
+
+	
+	public static int getNextId() {
+		return nextId;
+	}
+
+	
+	private void incNextId() {
+		nextId++;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+	
+	public void setName(String name) throws Exception {
+		if (name.length() > 50) {
+			throw new Exception("[ERROR] Station's name cannot be longer than 50 characters");
+		} else {
+			this.name = name;
+		}
+	}
+
+	
+	public Adress getAdress() {
+		return getAdress();
+	}
+
+
+	public void setAdress(String street, double latitude, double longitude) throws Exception {
+		
+	
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+	public void setCapacity(int capacity) throws Exception {
+		if (capacity <= 0) {
+			throw new Exception("[ERROR] Station's capacity must be greater than 0");
+		} else {
+			this.capacity = capacity;
+		}
+	}
+
+
+
+
+}
