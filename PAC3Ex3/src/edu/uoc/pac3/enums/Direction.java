679456f10ed60b18ef2bd14c4c157ec2e31c3c2a
ndev <napodevesa@gmail.com>
2020-10-22 07:24:13 +0200

    finish

--- PAC3Ex3/src/edu/uoc/pac3/enums/Direction.java
+++ PAC3Ex3/src/edu/uoc/pac3/enums/Direction.java
@@ -7,20 +7,53 @@ public enum Direction {
 	LEFT(180),
 	DOWN(270);
 	
+	
+
+	private int angle;
+
 	private Direction(int angle) {
-		
+		this.angle = angle;
 	}
 	
 	public int getAngle() {
-		//TODO
+		return this.angle;
 	}
 	
 	@Override
 	public String toString() {
-		//TODO
+		return "You are moving " + this.name();
 	}
 	
+	
 	public static Direction getDirection(int angle) {   	 
-		//TODO		
-	 }
-}
+		
+		while (angle >= 360) {
+			angle = angle - 360;
+		}	
+		
+			if (angle == 0 || angle > 315  && angle < 45) {
+				return RIGHT;
+			}
+			
+			if (angle == 90 || angle > 45 && angle < 135) {
+				return UP;
+			}
+			
+			if (angle == 180 || angle > 135 && angle < 225){
+				return LEFT;
+			}
+			
+			if (angle == 270 || angle > 225 && angle < 315) {
+				return DOWN;
+			}
+		
+
+		
+		return null;
+	
+	
+	}
+	
+	
+
+}	
